867cad1850a5e8da0ffa0024552babe288b5b387
Adam Orłowski <a.orlowski@onet.com.pl>
2024-11-04 16:49:47 +0100

    Dodaj pliki projektów.

---
+++ kameraa/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace kameraa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.buttonCapture = new System.Windows.Forms.Button();
+            this.pictureBoxDisplay = new System.Windows.Forms.PictureBox();
+            this.comboBoxCameras = new System.Windows.Forms.ComboBox();
+            this.buttonDisconnect = new System.Windows.Forms.Button();
+            this.buttonRecord = new System.Windows.Forms.Button();
+            this.buttonStopRecording = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDisplay)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(582, 79);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(75, 23);
+            this.buttonConnect.TabIndex = 0;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // buttonCapture
+            // 
+            this.buttonCapture.Location = new System.Drawing.Point(582, 130);
+            this.buttonCapture.Name = "buttonCapture";
+            this.buttonCapture.Size = new System.Drawing.Size(75, 23);
+            this.buttonCapture.TabIndex = 1;
+            this.buttonCapture.Text = "Capture";
+            this.buttonCapture.UseVisualStyleBackColor = true;
+            this.buttonCapture.Click += new System.EventHandler(this.buttonCapture_Click);
+            // 
+            // pictureBoxDisplay
+            // 
+            this.pictureBoxDisplay.Location = new System.Drawing.Point(109, 79);
+            this.pictureBoxDisplay.Name = "pictureBoxDisplay";
+            this.pictureBoxDisplay.Size = new System.Drawing.Size(438, 267);
+            this.pictureBoxDisplay.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBoxDisplay.TabIndex = 2;
+            this.pictureBoxDisplay.TabStop = false;
+            this.pictureBoxDisplay.Click += new System.EventHandler(this.pictureBoxDisplay_Click);
+            // 
+            // comboBoxCameras
+            // 
+            this.comboBoxCameras.FormattingEnabled = true;
+            this.comboBoxCameras.Location = new System.Drawing.Point(687, 129);
+            this.comboBoxCameras.Name = "comboBoxCameras";
+            this.comboBoxCameras.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxCameras.TabIndex = 3;
+            this.comboBoxCameras.SelectedIndexChanged += new System.EventHandler(this.comboBoxCameras_SelectedIndexChanged);
+            // 
+            // buttonDisconnect
+            // 
+            this.buttonDisconnect.Location = new System.Drawing.Point(687, 79);
+            this.buttonDisconnect.Name = "buttonDisconnect";
+            this.buttonDisconnect.Size = new System.Drawing.Size(117, 23);
+            this.buttonDisconnect.TabIndex = 4;
+            this.buttonDisconnect.Text = "Disconnect";
+            this.buttonDisconnect.UseVisualStyleBackColor = true;
+            this.buttonDisconnect.Click += new System.EventHandler(this.buttonDisconnect_Click);
+            // 
+            // buttonRecord
+            // 
+            this.buttonRecord.Location = new System.Drawing.Point(142, 374);
+            this.buttonRecord.Name = "buttonRecord";
+            this.buttonRecord.Size = new System.Drawing.Size(75, 23);
+            this.buttonRecord.TabIndex = 5;
+            this.buttonRecord.Text = "Record";
+            this.buttonRecord.UseVisualStyleBackColor = true;
+            this.buttonRecord.Click += new System.EventHandler(this.buttonRecord_Click);
+            // 
+            // buttonStopRecording
+            // 
+            this.buttonStopRecording.Location = new System.Drawing.Point(317, 373);
+            this.buttonStopRecording.Name = "buttonStopRecording";
+            this.buttonStopRecording.Size = new System.Drawing.Size(123, 24);
+            this.buttonStopRecording.TabIndex = 6;
+            this.buttonStopRecording.Text = "Stop recording";
+            this.buttonStopRecording.UseVisualStyleBackColor = true;
+            this.buttonStopRecording.Click += new System.EventHandler(this.buttonStopRecording_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(640, 198);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 56);
+            this.trackBar1.TabIndex = 7;
+            this.trackBar1.Tag = "";
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBarRed_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(640, 277);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(104, 56);
+            this.trackBar2.TabIndex = 8;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBarGreen_Scroll);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(640, 340);
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(104, 56);
+            this.trackBar3.TabIndex = 9;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBarBlue_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(802, 225);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 16);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Red";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(801, 277);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Green";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(804, 340);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 16);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Blue";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1036, 527);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.buttonStopRecording);
+            this.Controls.Add(this.buttonRecord);
+            this.Controls.Add(this.buttonDisconnect);
+            this.Controls.Add(this.comboBoxCameras);
+            this.Controls.Add(this.pictureBoxDisplay);
+            this.Controls.Add(this.buttonCapture);
+            this.Controls.Add(this.buttonConnect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDisplay)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.Button buttonCapture;
+        private System.Windows.Forms.PictureBox pictureBoxDisplay;
+        private System.Windows.Forms.ComboBox comboBoxCameras;
+        private System.Windows.Forms.Button buttonDisconnect;
+        private System.Windows.Forms.Button buttonRecord;
+        private System.Windows.Forms.Button buttonStopRecording;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ kameraa/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Accord.Video.FFMPEG;
+using AForge;
+using AForge.Imaging.Filters;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+// DETEKCJA RUCHU MA BYĆ REGULOWANA BO 2 KLATKI ZAWSZE BĘDĄ RÓŻNE, NO I KOLORY DODAĆ
+
+namespace kameraa
+{
+    public partial class Form1 : Form
+    {
+        public int red = 0;
+        public int green = 0;
+        public int blue = 0;
+        private FilterInfoCollection videoDevices;
+        private VideoCaptureDevice videoSource;
+        private VideoFileWriter videoWriter;
+        private bool isRecording = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadAvailableCameras();
+        }
+        private void LoadAvailableCameras() // Funkcja ładująca listę kamer do okienka rozwijanej listy
+        {
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice); // Pobranie listy kamer
+
+            foreach (FilterInfo device in videoDevices)
+            {
+                comboBoxCameras.Items.Add(device.Name); // Dodanie nazw kamer do ComboBox
+            }
+
+            if (comboBoxCameras.Items.Count > 0)
+            {
+                comboBoxCameras.SelectedIndex = 0; // Wybierz pierwszą kamerę na liście
+            }
+            else
+            {
+                MessageBox.Show("Nie znaleziono kamer USB.");
+            }
+        }
+
+
+        private void video_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+
+            Bitmap bitmap = (Bitmap)eventArgs.Frame.Clone();
+            pictureBoxDisplay.Image = ApplyRGBFilter(bitmap);
+
+            if (pictureBoxDisplay.Image != null)
+            {
+                pictureBoxDisplay.Image.Dispose();
+            }
+
+            pictureBoxDisplay.Image = (Bitmap)bitmap.Clone();
+
+            if (isRecording && videoWriter != null) // KONIECZNIE IF A NIE WHILE
+            {
+                videoWriter.WriteVideoFrame((Bitmap)bitmap.Clone());
+            }
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            if (videoSource != null && videoSource.IsRunning)
+            {
+                videoSource.SignalToStop();
+                videoSource.WaitForStop();
+            }
+
+            int selectedCameraIndex = comboBoxCameras.SelectedIndex;
+            if (selectedCameraIndex >= 0)
+            {
+                // Ustawienie źródła wideo na wybraną kamerę
+                videoSource = new VideoCaptureDevice(videoDevices[selectedCameraIndex].MonikerString);
+                videoSource.NewFrame += new NewFrameEventHandler(video_NewFrame);
+                videoSource.Start();
+            }
+        }
+
+        private void buttonCapture_Click(object sender, EventArgs e)
+        {
+            if (pictureBoxDisplay.Image != null)
+            {
+                Bitmap image = new Bitmap(pictureBoxDisplay.Image);
+                image.Save(@"C:\Users\aorlo\Desktop\photo.png", ImageFormat.Png);
+                image.Dispose();
+            }
+        }
+
+        private void pictureBoxDisplay_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBoxCameras_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            if (videoSource != null && videoSource.IsRunning)
+            {
+                videoSource.SignalToStop();
+                videoSource.WaitForStop();
+            }
+            base.OnFormClosing(e);
+        }
+
+        private void buttonDisconnect_Click(object sender, EventArgs e)
+        {
+            if (videoSource != null && videoSource.IsRunning)
+            {
+                videoSource.SignalToStop();
+                videoSource.WaitForStop();
+                videoSource = null;
+                pictureBoxDisplay.Image = null;
+            }
+        }
+
+        private void buttonRecord_Click(object sender, EventArgs e)
+        {
+                if (!isRecording)
+                {
+                    string videoFilePath = @"C:\Users\aorlo\Desktop\video.avi";
+
+                    videoWriter = new VideoFileWriter();
+                    videoWriter.Open(videoFilePath, 450, 300);
+
+                    isRecording = true;
+                }
+        }
+
+        private void buttonStopRecording_Click(object sender, EventArgs e)
+        {
+            if (isRecording)
+            {
+                isRecording = false;
+                videoWriter.Close();
+                videoWriter = null;
+            }
+        }
+        private void SetTrackBarProperties()
+        {
+            trackBar1.Maximum = 255;
+            trackBar2.Maximum = 255;
+            trackBar3.Maximum = 255;
+
+            trackBar1.Minimum = 0;
+            trackBar2.Minimum = 0;
+            trackBar3.Minimum = 0;
+
+            trackBar1.TickFrequency = 3;
+            trackBar2.TickFrequency = 3;
+            trackBar3.TickFrequency = 3;
+        }
+
+        private Bitmap ApplyRGBFilter(Bitmap sourceImage)
+        {
+            ColorFiltering filter = new ColorFiltering();
+            filter.Red = new IntRange(0, red);      
+            filter.Green = new IntRange(0, green);  
+            filter.Blue = new IntRange(0, blue);    
+            Bitmap processedImage = filter.Apply(sourceImage);
+            return processedImage;
+        }
+
+        private void trackBarRed_Scroll(object sender, EventArgs e)
+        {
+            red = trackBar1.Value;
+        }
+
+        private void trackBarGreen_Scroll(object sender, EventArgs e)
+        {
+            green = trackBar1.Value;
+        }
+
+        private void trackBarBlue_Scroll(object sender, EventArgs e)
+        {
+            blue = trackBar1.Value;
+        }
+    }
+}
